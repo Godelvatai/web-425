9ffe8102d6a631d478d4017171af194b4662b8d6
Godelvatai <godelvatai@gmail.com>
2024-08-25 21:27:41 -0400

    Week 2 Assignment
    Week 2 Assignment

--- rpg-character-builder/src/app/app.component.ts
+++ rpg-character-builder/src/app/app.component.ts
@@ -11,11 +11,18 @@ import { RouterLink, RouterOutlet } from '@angular/router';
         <img src="/assets/rpg-character-builder-heading.png" alt="website banner for rpg character builder" class="banner-img">
       </header>
 
+      <div class="sign-in-container">
+        <a routerLink="/signin" class="sign-in-link">Sign In</a>
+      </div>
+
       <main class="main-content">
         <nav class="navbar">
           <ul>
             <li><a routerLink="/">Home</a></li>
-            <li><a href="#">Character Creator</a></li>
+            <li><a routerLink="/players">Players</a></li>
+            <li><a routerLink="/create-character">Create Character</a></li>
+            <li><a routerLink="/create-guild">Create Guild</a></li>
+            <li><a routerLink="/create-faction">Create Faction</a></li>
           </ul>
         </nav>
 
@@ -27,14 +34,31 @@ import { RouterLink, RouterOutlet } from '@angular/router';
       <footer class="footer">
         <nav class="footer-nav">
           <a routerLink="/">Home</a>
-          <a href="#">Character Creator</a>
+          <a routerLink="/players">Players</a>
+          <a routerLink="/create-character">Create Character</a>
+          <a routerLink="/create-guild">Create Guild</a>
+          <a routerLink="/create-faction">Create Faction</a>
         </nav>
       </footer>
     </div>
   `,
   styles: [
     `
+    .sign-in-container{
+      text-align: right;
+      padding-right: 20px;
+      margin-top: 10px;
+    }
 
+    .sign-in-link{
+      color: #000000;
+      text-decoration: none;
+      font-family: 'Lato', sans-serif;
+    }
+
+    .sign-in-link:hover{
+      text-decoration: underline;
+    }
     `
   ]
 })
