f21c8243fb9c02219fa6ef527e69e77c5fa8cc2b
Godelvatai <godelvatai@gmail.com>
2024-09-30 00:41:51 -0400

    Week 4 Assignment
    Week 4 Assignment

--- rpg-character-builder/src/app/create-character/create-character.component.spec.ts
+++ rpg-character-builder/src/app/create-character/create-character.component.spec.ts
@@ -20,4 +20,39 @@ describe('CreateCharacterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should generate a random character ID between 1 and 1000 with no decimal places', () => {
+    component.createNewCharacter(); //This will trigger the generation of a new character ID
+    expect(component.characterId).toBeGreaterThan(0);
+    expect(component.characterId).toBeLessThanOrEqual(1000);
+    expect(Number.isInteger(component.characterId)).toBe(true);
+  });
+
+  it('should add a new character with the correct customization', () => {
+    component.characterId = 12;
+    component.name = "Jane Doe"
+    component.selectedGender = 'Female';
+    component.selectedClass = 'Mage';
+    component.addToCharacterList();
+    const newCharacter = component.characterList.characters[0];
+
+    expect(newCharacter.id).toBe(12);
+    expect(newCharacter.name).toBe('Jane Doe');
+    expect(newCharacter.gender).toBe('Female');
+    expect(newCharacter.class).toBe('Mage');
+  });
+
+  it('should reset all form fields to their default values after resetForm is called', () => {
+    component.characterId = 12;
+    component.name = 'Jane Doe';
+    component.selectedGender = 'Female';
+    component.selectedClass = 'Mage';
+
+    component.resetForm();
+
+    expect(component.characterId).toBe(1);
+    expect(component.name).toBe('');
+    expect(component.selectedGender).toBe('');
+    expect(component.selectedClass).toBe('');
+  });
 });
