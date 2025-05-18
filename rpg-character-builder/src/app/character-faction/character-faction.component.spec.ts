47fccb34a06468b455fc86d09d11b9315113c04e
Godelvatai <godelvatai@gmail.com>
2025-05-18 03:57:06 -0400

    Week 8 Assignment

--- rpg-character-builder/src/app/character-faction/character-faction.component.spec.ts
+++ rpg-character-builder/src/app/character-faction/character-faction.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CharacterFactionComponent } from './character-faction.component';
 
 describe('CreateFactionComponent', () => {
   let component: CharacterFactionComponent;
   let fixture: ComponentFixture<CharacterFactionComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharacterFactionComponent]
+      imports: [ CharacterFactionComponent, HttpClientTestingModule ]
     })
     .compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
 
     fixture = TestBed.createComponent(CharacterFactionComponent);
     component = fixture.componentInstance;
@@ -19,5 +23,44 @@ describe('CreateFactionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle errors when data is not found.', () => {
+    const expectedMessage = "Error fetching character factions.";
+    const req = httpTestingController.expectOne(`http://localhost:3000/api/character-factions`);
+    req.flush('faction not found', { status: 404, statusText: 'Not Found' });
+
+    expect(component.noSpecialMessage).toEqual(expectedMessage);
+    httpTestingController.verify();
+  });
+
+  it('should correctly fetch a list of character factions.', () => {
+    const mockFactions = [{
+      name: "Shroomite Tinkerers",
+      description: "Where machine meets mushroom. Bio-engineering but with magic and old science."
+    },{
+      name: "Desert Thorns",
+      description: "Just a bunch of cacti."
+    }];
+
+    const req = httpTestingController.expectOne(`http://localhost:3000/api/character-factions`);
+    req.flush(mockFactions);
+
+    expect(component.factions.length).toBe(2);
+    expect(component.factions).toEqual(mockFactions);
+    httpTestingController.verify();
+  });
+
+  it('should update the characterFactions div when character factions are found.', () => {
+    const mockFactions = [{
+      name: "Shroomite Tinkerers",
+      description: "Where machine meets mushroom. Bio-engineering but with magic and old science."
+    }];
+
+    const req = httpTestingController.expectOne(`http://localhost:3000/api/character-factions`);
+    req.flush(mockFactions);
+
+    expect(component.factions).toEqual(mockFactions);
+    httpTestingController.verify();
   });
 });
--- rpg-character-builder/src/app/character-faction/character-faction.component.ts
+++ rpg-character-builder/src/app/character-faction/character-faction.component.ts
@@ -1,16 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-character-faction',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   template: `
-    <p>
-      character-faction works!
-    </p>
+    <div class="cfaction-container">
+      <div *ngIf="factions; else noFactions">
+        <h1>Character Factions</h1>
+        <div *ngFor="let faction of factions" class="cfaction-card">
+          <h3>{{ faction.name }}</h3>
+          <p>{{ faction.description }}</p>
+        </div>
+      </div>
+      <ng-template #noFactions>
+        <h1>{{ noSpecialMessage }}</h1>
+      </ng-template>
+    </div>
   `,
-  styles: ``
+  styles: `
+    .cfaction-container {
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+    }
+
+    .cfaction-card {
+      margin: 10px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      padding: 20px;
+      width: 80%;
+    }
+  `
 })
 export class CharacterFactionComponent {
+  factions: any = null;
+  noSpecialMessage: string = '';
 
+  constructor(private http: HttpClient) {
+    this.http.get(`http://localhost:3000/api/character-factions`).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.factions = res;
+      },
+      error: (err) => {
+        console.error('Error fetching character factions', err);
+        if(err.error === 'Factions not found') {
+          this.noSpecialMessage = 'No character factions were found.'
+        } else {
+          this.noSpecialMessage = 'Error fetching character factions.'
+        }
+      }
+    });
+  }
 }
